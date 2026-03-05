39758dc0e90ab811451f9268b286d28f84f16ab2
Tharindu Attygalle <tharinduattygalle@Tharindus-MacBook-Air.local>
2026-03-05 19:22:01 +0530

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://api.apsc2026.lk/api',
-    apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://api.apsc2026.lk/api',
   publicAppUrl: 'https://portal.apsc2026.lk',
 };
